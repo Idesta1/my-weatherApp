0913a61abbb1afec3d9e7356ed377114ef6d90d9
Idesta1 <116448334+Idesta1@users.noreply.github.com>
2024-09-24 14:39:43 +0200

    updated axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+let apiKey="8e95b71ct703a6fac0458b466ebo4b0b";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=mertic`;
+
+
 function showSubmit(event){
     event.preventDefault();
   let searchInput=document.querySelector("#search-input-app");
