72c22c4fe8330b0207f8f83f68624dcf7f91a8ca
Idesta1 <116448334+Idesta1@users.noreply.github.com>
2024-09-24 15:38:13 +0200

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-let apiKey="8e95b71ct703a6fac0458b466ebo4b0b";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=mertic`;
+function changeWeather(response){
+let temperatureElement=document.querySelector("#temperature");
+let temperature =response.data.temperature.current;
+let cityElement =document.querySelector("#city");
 
+temperatureElement.innerHTML=Math.round(temperature);
+cityElement.innerHTML=response.data.city;
+}
+
+function searchCity(city){
+let apiKey="8e95b71ct703a6fac0458b466ebo4b0b";
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(changeWeather);
+}
 
 function showSubmit(event){
     event.preventDefault();
   let searchInput=document.querySelector("#search-input-app");
- let cityElement =document.querySelector("#city");
-   cityElement.innerHTML=searchInput.value;
+
+   
+   searchCity(searchInput.value);
 
 }
 
 
 let searchFormElement=document.querySelector("#search-form-app");
 searchFormElement.addEventListener ("submit", showSubmit);
+
+searchCity("Paris");
